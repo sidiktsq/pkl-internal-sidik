3ecb5e4055774f6dc5c392b3287676d32cde5b60
sidiktsq <baika3543@gmail.com>
2025-12-15 15:51:34 +0700

    perubahan di web

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tentang', function () {
+    return view('tentang');
+});
+
+Route::get('/sapa/{nama?}', function ($nama ='semua') {
+    // ↑ '/sapa/{nama}' = URL pattern
+    // ↑ {nama}         = Parameter dinamis, nilainya dari URL
+    // ↑ function($nama) = Parameter diterima di function
+
+    return "Halo, $nama! Selamat datang di toko Fashion SMK😍😍.";
+    // ↑ "$nama" = Variable interpolation (masukkan nilai $nama ke string)
+});
+
+Route::get('/produk/{id}', function ($id) {
+    return "Detail produk #$id";
+})->name('produk.detail');
